c142a7debd6459f0c517217454f1f8840d4d62b3
Nick B <nbourrereg@hotmail.com>
2020-03-02 14:05:44 -0500

    Ajout du projet avec les rangées en alternance

---
+++ data_models/IDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_models
+{
+    interface IDataService<T>
+    {
+        IEnumerable<T> GetAll();
+    }
+}
---
+++ datagrid_alternate_rows/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using data_models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace datagrid_alternate_rows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        ObservableCollection<Customer> customers;
+        
+        public ObservableCollection<Customer> Customers {
+            get => customers;
+
+            set
+            {
+                customers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initValues();
+        }
+
+
+        private void initValues()
+        {
+            Customers = new ObservableCollection<Customer>(new CustomerDataService().GetAll());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
